9e6f6e6305a88032d90fab7901de6c534c5d5ca4
sharp <sharp.gan1993@gmail.com>
2020-01-17 19:46:36 +0800

    update

--- core/config.py
+++ core/config.py
@@ -1,6 +1,6 @@
-sender =''
-receiver=''
-pwd = ''
-leancloud_appid=''
-leancloud_appkey=''
-interval=300
+sender ='' # 提供smtp服务的邮箱，我采用的是126的 
+receiver='' # 接收邮件通知的邮箱 
+pwd = '' # smtp服务的邮箱密码
+leancloud_appid='' # leancloud的应用appid，可在控制面板的 设置-应用keys 中找到
+leancloud_appkey='' # leancloud的应用appkey，可在控制面板的 设置-应用keys 中找到
+interval=300 # 每隔多少秒执行一下检测是否有新评论，这里默认给了300秒
