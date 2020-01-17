09f29e81a77f73da0d4b2cea02ae6fe81d4e5fc9
sharp <sharp.gan1993@gmail.com>
2020-01-17 16:54:11 +0800

    update

--- notifier.py
+++ notifier.py
@@ -7,7 +7,7 @@
 """
 
 import leancloud
-#from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from core import helper, config
 
 class Misc(object):
@@ -52,7 +52,6 @@ class Query(object):
 
 if __name__ == '__main__':
     q=Query()
-    q.main()
-    #scheduler = BlockingScheduler()
-    #scheduler.add_job(q.main, 'interval', seconds=config.interval)
-    #scheduler.start()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(q.main, 'interval', seconds=config.interval)
+    scheduler.start()
